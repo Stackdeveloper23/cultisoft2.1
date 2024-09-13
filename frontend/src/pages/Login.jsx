cc3d91502d331162e80ca9458c3cd8dbafa530a9
Stackdeveloper23 <vargasrojas663@gmail.com>
2024-09-13 09:59:08 -0500

    cookie

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
   const submitLogin = async (e) => {
     e.preventDefault();
-    await axios.get("/santum/csrf-cookie").then(() => {
+    await axios.get("https://backendcultisoft.azurewebsites.net/sanctum/csrf-cookie").then(() => {
       Config.getLogin({ email, password }).then(({ data }) => {
         if (data.success) {
           console.log(data);
